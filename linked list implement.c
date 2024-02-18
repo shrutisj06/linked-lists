9e2b9905959cc1c664e4430fc79832333860a3c8
shrutisj06 <157938791+shrutisj06@users.noreply.github.com>
2024-02-18 11:08:36 +0530

    Add files via upload

---
+++ linked list implement.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include<stdlib.h>
+struct node{
+int data;
+struct node*next;
+};
+
+void linkedlisttraversal(struct node*ptr)
+{   while(ptr!= NULL)
+    {printf("Element:%d\n",ptr->data);
+    ptr=ptr->next;
+}
+}
+
+int main()
+{
+    struct node*head;
+    struct node*second;
+    struct node*third;
+    struct node*fourth;
+    head=(struct node*)malloc(sizeof(struct node));
+    second=(struct node*)malloc(sizeof(struct node));
+    third=(struct node*)malloc(sizeof(struct node));
+    fourth=(struct node*)malloc(sizeof(struct node));
+
+//link first and second nodes
+head->data=7;
+head->next=second;
+
+//link second and third node
+second->data=11;
+second->next=third;
+
+//link third and fourth node
+third->data=66;
+third->next=fourth;
+
+//terminate at fourth node
+fourth->data=90;
+fourth->next=NULL;
+linkedlisttraversal(head);
+return 0;
+}
